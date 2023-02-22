36d82af13ddb8a749340fe049fd8facd4ab09c36
queensk <queenskisivuli@gmail.com>
2023-02-22 18:45:36 +0300

    approve cetificate navigation

--- frontend/src/pages/UserPage/UserPage.jsx
+++ frontend/src/pages/UserPage/UserPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import { UserProfile } from "../../components/UserProfile";
+import UserCertificates from "../../components/UserCertificates/UserCertificates";
+import ApprovedCertificate from "../../components/ApprovedCertificate/ApprovedCertificate";
 import "./UserPage.css";
 
 export default function UserPage() {
@@ -13,8 +15,11 @@ export default function UserPage() {
   return (
     <div className="user-data">
       <NavBar handleMenuClick={handleMenuClick} activeMenu={activeMenu} />
+      <div className="dummy-nav"></div>
       <div className="User-Main-content">
         {activeMenu === "UserProfile" && <UserProfile />}
+        {activeMenu === "UserCertificates" && <UserCertificates />}
+        {activeMenu === "ApprovedCertificate" && <ApprovedCertificate />}
       </div>
     </div>
   );
