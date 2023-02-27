059e31faac21f672a6c58a5a5b99519372886b75
queensk <queenskisivuli@gmail.com>
2023-02-27 15:29:50 +0300

    models basemodel init

---
+++ Backend/app/models/base_model.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+"""
+Contains class BaseModel
+"""
+
+from datetime import datetime
+import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
+
+
+class BaseModel:
+    """
+    The BaseModel class from which future classes will be derived
+    """
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+
+            if kwargs.get(
+                    "created_at",
+                    None) and isinstance(
+                    self.created_at,
+                    str):
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+
+            if kwargs.get(
+                    "update_at",
+                    None) and isinstance(
+                    self.updated_at,
+                    str):
+                self.updated_at = datetime.strptime(kwargs["updated_at"], str)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("id", None) and isinstance(self.id, str):
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """
+        Base model string representation
+        """
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
+
+    def save(self):
+        """
+        Update base attributes to the db
+        """
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save(self)
+
+    def to_dict(self, save_fs=None):
+        """
+        Return dict contains attribute of object state
+        """
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        if save_fs is None:
+            if "password" in new_dict:
+                del new_dict["password"]
+        return new_dict
+
+    def delete(self):
+        """
+        Delete instate from storage
+        """
+        models.storage.delete(self)
