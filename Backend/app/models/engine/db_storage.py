31d987b3a3160af1285256a134f7b9574ce38372
queensk <queenskisivuli@gmail.com>
2023-03-01 09:15:52 +0300

    db storage helper methods

--- Backend/app/models/engine/db_storage.py
+++ Backend/app/models/engine/db_storage.py
@@ -3,15 +3,17 @@
 Db Storage module
 """
 
-import models
-from models.base_model import BaseModel, Base
-from models.user import User
+from app import models
+from app.models.base_model import BaseModel, Base
+from app.models.user import User
+from app.models.school import School
+from app.models.certificate import Certificate
 from os import getenv
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-classes = {"User": User}
+classes = {"User": User, "School": School, "Certificate": Certificate}
 
 
 class DBstorage:
@@ -39,79 +41,79 @@ class DBstorage:
         if BCV_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
-        def all(self, cls=None):
-            """
-            Get all the db sessions
-            """
-            new_dict = {}
-            for clss in classes:
-                if cls is None or cls is classes[clss] or cls is clss:
-                    objs = self.__session.query(classes[clss]).all()
-                    for obj in objs:
-                        key = obj.__class__.__name__ + '.' + obj.id
-                        new_dict[key] = obj
-            return (new_dict)
+    def all(self, cls=None):
+        """
+        Get all the db sessions
+        """
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
 
-        def add(self, obj):
-            """
-            Add object to current database
-            """
-            self.__session.add(obj)
+    def add(self, obj):
+        """
+        Add object to current database
+        """
+        self.__session.add(obj)
 
-        def save(self):
-            """
-            Save object to db
-            """
-            self.__session.commit()
+    def save(self):
+        """
+        Save object to db
+        """
+        self.__session.commit()
 
-        def delete(self, obj=None):
-            """
-            Delete Object from db
-            """
-            if obj is not None:
-                self.__session.delete(obj)
+    def delete(self, obj=None):
+        """
+        Delete Object from db
+        """
+        if obj is not None:
+            self.__session.delete(obj)
 
-        def reload(self):
-            """
-            Reload data from the database
-            """
-            Base.metadata.create_all(self.__engine)
-            sess_factory = sessionmaker(
-                bind=self.__engine, expire_on_commit=False)
-            Session = scoped_session(sess_factory)
-            self.__session = Session
+    def reload(self):
+        """
+        Reload data from the database
+        """
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
 
-        def close(self):
-            """
-            Removes session
-            """
-            self.__session.remove()
+    def close(self):
+        """
+        Removes session
+        """
+        self.__session.remove()
 
-        def get(self, cls, id):
-            """
-            Return objects based on classname and Id
-            """
-            if cls not in classes.values():
-                return None
-
-            all_cls = models.storage.all(cls)
-            for value in all_cls.values():
-                if (value.id == id):
-                    return value
-
+    def get(self, cls, id):
+        """
+        Return objects based on classname and Id
+        """
+        if cls not in classes.values():
             return None
 
-        def count(self, cls=None):
-            """
-            Count objects
-            """
-            all_class = classes.values()
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return value
 
-            if not cls:
-                count = 0
-                for clas in all_class:
-                    count += len(models.storage.all(clas).values())
-            else:
-                count = len(models.storage.all(cls).values())
+        return None
 
-            return count
+    def count(self, cls=None):
+        """
+        Count objects
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
