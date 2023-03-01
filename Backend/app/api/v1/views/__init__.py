3146b28ece041b7ade42bdeb9b49a689aeb7f6c6
queensk <queenskisivuli@gmail.com>
2023-03-01 09:22:58 +0300

    api blueprint init

---
+++ Backend/app/api/v1/views/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Blueprint for API
+"""
+
+
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from app.api.v1.views.schools import *
+from app.api.v1.views.users import *
+from app.api.v1.views.index import *
+from app.api.v1.views.certificates import *
