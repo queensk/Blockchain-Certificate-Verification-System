4bc03b1e04151cba087b726f2601d4c86501910f
queensk <queenskisivuli@gmail.com>
2023-03-01 09:23:33 +0300

    api index for smoke test

---
+++ Backend/app/api/v1/views/index.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Index api route
+"""
+from flask import jsonify
+from app.models.user import User
+from app.models.school import School
+from app.models.certificate import Certificate
+from app.models import storage
+from app.api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """
+    Status of API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """
+    Retrieves the number of each objects by type
+    """
+    classes = [User, School, Certificate]
+    names = ["users", "schools", "certificate"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
