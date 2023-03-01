344d51e644db5d4ebf94af39416b6e118d11f86b
queensk <queenskisivuli@gmail.com>
2023-03-01 09:09:07 +0300

    models import fix

--- Backend/app/models/base_model.py
+++ Backend/app/models/base_model.py
@@ -4,7 +4,7 @@ Contains class BaseModel
 """
 
 from datetime import datetime
-import models
+from app import models
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, DateTime
