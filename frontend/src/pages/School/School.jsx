0353087a6df36c071c3a5843d494c828604a2d09
queensk <queenskisivuli@gmail.com>
2023-03-13 07:47:47 +0300

    school dashboard page

---
+++ frontend/src/pages/School/School.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import { UserProfile } from "../../components/UserProfile";
+import UserCertificates from "../../components/UserCertificates/UserCertificates";
+import ApprovedCertificate from "../../components/ApprovedCertificate/ApprovedCertificate";
+import PendingApproval from "../../components/PendingApproval/PendingApproval";
+import About from "../../components/About/About";
+import "./School.css";
+
+export default function School({
+  userId,
+  firstName,
+  lastName,
+  setToken,
+  setAuthenticated,
+}) {
+  const [activeMenu, setActiveMenu] = useState("");
+
+  const handleMenuClick = (menu) => {
+    setActiveMenu(menu);
+  };
+
+  return (
+    <div className="user-data">
+      school
+      <NavBar
+        handleMenuClick={handleMenuClick}
+        activeMenu={activeMenu}
+        setToken={setToken}
+        setAuthenticated={setAuthenticated}
+      />
+      <div className="dummy-nav"></div>
+      <div className="User-Main-content">
+        {activeMenu === "UserProfile" && <UserProfile userId={userId} />}
+        {activeMenu === "UserCertificates" && (
+          <UserCertificates
+            userId={userId}
+            firstName={firstName}
+            lastName={lastName}
+          />
+        )}
+        {activeMenu === "ApprovedCertificate" && <ApprovedCertificate />}
+        {activeMenu === "PendingApproval" && <PendingApproval />}
+        {activeMenu === "About" && <About />}
+      </div>
+    </div>
+  );
+}
