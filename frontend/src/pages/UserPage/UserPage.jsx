d5ba17490b915d352f9e86fb9a7e15e140b9a00c
queensk <queenskisivuli@gmail.com>
2023-03-13 07:52:47 +0300

    user page componets

--- frontend/src/pages/UserPage/UserPage.jsx
+++ frontend/src/pages/UserPage/UserPage.jsx
@@ -4,9 +4,16 @@ import { UserProfile } from "../../components/UserProfile";
 import UserCertificates from "../../components/UserCertificates/UserCertificates";
 import ApprovedCertificate from "../../components/ApprovedCertificate/ApprovedCertificate";
 import PendingApproval from "../../components/PendingApproval/PendingApproval";
+import About from "../../components/About/About";
 import "./UserPage.css";
 
-export default function UserPage() {
+export default function UserPage({
+  userId,
+  firstName,
+  lastName,
+  setToken,
+  setAuthenticated,
+}) {
   const [activeMenu, setActiveMenu] = useState("");
 
   const handleMenuClick = (menu) => {
@@ -15,13 +22,25 @@ export default function UserPage() {
 
   return (
     <div className="user-data">
-      <NavBar handleMenuClick={handleMenuClick} activeMenu={activeMenu} />
+      <NavBar
+        handleMenuClick={handleMenuClick}
+        activeMenu={activeMenu}
+        setToken={setToken}
+        setAuthenticated={setAuthenticated}
+      />
       <div className="dummy-nav"></div>
       <div className="User-Main-content">
-        {activeMenu === "UserProfile" && <UserProfile />}
-        {activeMenu === "UserCertificates" && <UserCertificates />}
+        {activeMenu === "UserProfile" && <UserProfile userId={userId} />}
+        {activeMenu === "UserCertificates" && (
+          <UserCertificates
+            userId={userId}
+            firstName={firstName}
+            lastName={lastName}
+          />
+        )}
         {activeMenu === "ApprovedCertificate" && <ApprovedCertificate />}
         {activeMenu === "PendingApproval" && <PendingApproval />}
+        {activeMenu === "About" && <About />}
       </div>
     </div>
   );
