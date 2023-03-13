2d46c5d41af1e5b911bfaa10f6e13ad1eb14b091
queensk <queenskisivuli@gmail.com>
2023-03-13 07:42:33 +0300

    commection to base url

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import TextInput from "../TextInput/TextInput";
-import api from "../../api/api";
+import api from "../../api/api.jsx";
 import MessagePopUp from "../MessagePopUp/MessagePopUp";
 
 export default function SignUpInput() {
