3edc033b4779ba8d89ec84437d6a66208306e245
queensk <queenskisivuli@gmail.com>
2023-04-04 10:20:42 +0300

    unauthorized. users

---
+++ frontend/src/components/Unauthorized/Unauthorized.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Unauthorized.css";
+
+const Unauthorized = () => {
+  return (
+    <div className="unauthorized-container">
+      <h1 className="unauthorized-text">Unauthorized</h1>
+    </div>
+  );
+};
+
+export default Unauthorized;
