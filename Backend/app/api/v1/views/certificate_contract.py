6cd3aae5564d68a9f9321f38ea3836cf83485737
queensk <queenskisivuli@gmail.com>
2023-04-04 10:53:36 +0300

    update accoutn and certificate data

--- Backend/app/api/v1/views/certificate_contract.py
+++ Backend/app/api/v1/views/certificate_contract.py
@@ -5,10 +5,15 @@ Certificate connection route to contract
 from web3 import Web3
 from flask import jsonify, request, abort
 import json
+import os
+from datetime import datetime
+from app.models import storage
+from app.models.certificate import Certificate
+from app.models.certificate_request_verification import VerifyCertificate
 from app.api.v1.views import app_views
 from app.api.v1.views.utilities.encode import generate_keccak256_hash
 from eth_abi import decode
-
+    
 # Initialize Flask app and Web3 provider
 with open('/home/queens/Blockchain-Certificate-Verification-System/Backend/contracts/artifacts/contracts/Certificate.sol/CertificateVerifier.json') as f:
     certificate_json = json.load(f)
@@ -26,8 +31,8 @@ contract = w3.eth.contract(
 # Flask routes
 
 
-@app_views.route('/add-certificate', methods=['POST'])
-def add_contract_certificate():
+@app_views.route('/add-certificate/<certificate_id>', methods=['POST'])
+def add_contract_certificate(certificate_id):
     """
     Add new certificate to contract
     """
@@ -36,18 +41,21 @@ def add_contract_certificate():
         if not request.get_json():
             abort(400, description="Not a JSON")
 
-        certificate_data = request.json
-        name = certificate_data['student_name']
-        school_name = certificate_data['school_name']
-        school_major = certificate_data['school_major']
-        school_department = certificate_data['school_department']
+        data = request.get_json()
+        certificate = storage.get(Certificate, certificate_id)
+        # certificate_data = request.json
+        name = certificate.student_name
+        school_name = certificate.school_name
+        school_major = certificate.school_major
+        school_department = certificate.school_department
 
         # Generate the certificate hash
-        certificate_data_string = f"{name}{school_name}{school_major}{school_department}"
+        certificate_data_string = f"{name}{school_name}{school_major}{school_department}{certificate_id}{datetime.utcnow()}{os.environ['SECRET_KEY']}"
         certificate_hash = generate_keccak256_hash(certificate_data_string)
 
         # Get the account that will send the transaction
-        account = w3.eth.accounts[0]
+        # account = w3.eth.accounts[0]
+        account = Web3.to_checksum_address(data['account'])
 
         # Build the transaction to add the certificate to the contract
         tx_hash = contract.functions.addCertificate(
@@ -70,6 +78,7 @@ def add_contract_certificate():
         if not certificate_info:
             abort(404, description="Certificate not created successfully")
 
+
         # Construct the response JSON
         response = {
             'certificate_hash': certificate_info[0].hex(),
@@ -79,8 +88,13 @@ def add_contract_certificate():
             'school_department': certificate_info[4],
             'verified': certificate_info[5],
         }
+        setattr(certificate, "certificate_hash", certificate_info[0].hex())
+        certificate_verification_status = storage.get_certificates_status(VerifyCertificate, certificate.id)
+        setattr(certificate_verification_status, "certificate_status", "pending")
+        storage.save()
+    
 
-        return jsonify({'success': True, 'certificate_data': response})
+        return jsonify({'success': True, 'certificate_data': certificate.to_dict()})
 
     except ValueError as e:
         abort(400, description=str(e))
@@ -121,12 +135,15 @@ def verify_contract_certificate(certificate_hash):
     Verify a certificate
     """
     try:
-        account = w3.eth.accounts[0]
+        data = request.get_json()
+        # account = w3.eth.accounts[0]
+        certificate = storage.get(Certificate, data['certificate_id'])
+        account = Web3.to_checksum_address(data['account'])
         tx_hash = contract.functions.verifyCertificate(
             bytes.fromhex(certificate_hash)).transact({'from': account, })
         receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
         if not receipt:
-            return jsonify(
+            return jsonify(                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
                 {'success': False, 'message': 'Transaction failed to execute.'}), 400
         certificate_info = contract.functions.getCertificateInfo(
             bytes.fromhex(certificate_hash)).call()
@@ -138,6 +155,16 @@ def verify_contract_certificate(certificate_hash):
             'school_department': certificate_info[4],
             'certificate_verify': certificate_info[5],
         }
+        certificate_data = certificate.to_dict()
+
+        for key, value in certificate_data.items():
+            if key not in response:
+                response["key"] = value
+        setattr(certificate, "verified_certificate", certificate_info[5])
+        certificate_verification_status = storage.get_certificates_status(VerifyCertificate, certificate.id)
+        setattr(certificate_verification_status, "certificate_status", "verified")
+        storage.save()
+
         return jsonify({'success': True, 'verified': response})
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)}), 400
