d4834d697d4deb038b2cf049d34544b1063a46d9
queensk <queenskisivuli@gmail.com>
2023-04-04 10:23:58 +0300

    user profile update

--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import api from "../../api/api.jsx";
 import User from "./User";
 import ErrorPopUp from "../MessagePopUp/MessagePopUp";
+import LoadingAnimation from "../Loading/Loading.jsx";
 
 export default function UserProfile({ userId }) {
   const [error, setError] = useState(false);
   const [user, setUserData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     api
@@ -15,10 +17,14 @@ export default function UserProfile({ userId }) {
       })
       .catch((error) => {
         setError("An error occurred while connecting to the server!");
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, [userId]);
   return (
     <>
+      {isLoading && <LoadingAnimation />}
       {error && <ErrorPopUp message={error} setMessage={setError} />}
       <div className="user-profile-container">
         <User {...user} setUserData={setUserData} />
