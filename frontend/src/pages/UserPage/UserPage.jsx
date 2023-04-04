9fc5f6fc7f3ad412167f8f442c226a17c6b23fc2
queensk <queenskisivuli@gmail.com>
2023-04-04 10:43:07 +0300

    user page

--- frontend/src/pages/UserPage/UserPage.jsx
+++ frontend/src/pages/UserPage/UserPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import { UserProfile } from "../../components/UserProfile";
 import UserCertificates from "../../components/UserCertificates/UserCertificates";
@@ -6,16 +6,20 @@ import ApprovedCertificate from "../../components/ApprovedCertificate/ApprovedCe
 import PendingApproval from "../../components/PendingApproval/PendingApproval";
 import About from "../../components/About/About";
 import "./UserPage.css";
+import { AuthContext } from "../../CustomHooks/Context/AuthProvider";
+import Footer from "../../components/Footer/Footer";
 
-export default function UserPage({
-  userId,
-  firstName,
-  lastName,
-  setToken,
-  setAuthenticated,
-}) {
-  const [activeMenu, setActiveMenu] = useState("");
-
+export default function UserPage() {
+  const [activeMenu, setActiveMenu] = useState("UserProfile");
+  const {
+    authenticated,
+    userId,
+    firstName,
+    lastName,
+    userRole,
+    setToken,
+    setAuthenticated,
+  } = useContext(AuthContext);
   const handleMenuClick = (menu) => {
     setActiveMenu(menu);
   };
@@ -38,9 +42,14 @@ export default function UserPage({
             lastName={lastName}
           />
         )}
-        {activeMenu === "ApprovedCertificate" && <ApprovedCertificate />}
-        {activeMenu === "PendingApproval" && <PendingApproval />}
+        {activeMenu === "ApprovedCertificate" && (
+          <ApprovedCertificate userId={userId} />
+        )}
+        {activeMenu === "PendingApproval" && (
+          <PendingApproval userId={userId} />
+        )}
         {activeMenu === "About" && <About />}
+        <Footer />
       </div>
     </div>
   );
