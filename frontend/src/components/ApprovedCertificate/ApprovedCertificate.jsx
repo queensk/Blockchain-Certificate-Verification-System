f2257b2801f84afca2dbf9309b77e9160776dd95
queensk <queenskisivuli@gmail.com>
2023-04-04 09:59:10 +0300

    update aproved certificate handler

--- frontend/src/components/ApprovedCertificate/ApprovedCertificate.jsx
+++ frontend/src/components/ApprovedCertificate/ApprovedCertificate.jsx
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Certificate from "../Certificate/Certificate";
+import api from "../../api/api";
+import LoadingAnimation from "../Loading/Loading";
 
-export default function ApprovedCertificate() {
-  const certificates = [
-    {
-      student_name: "John Smith",
-      student_email: "john.smith@example.com",
-      school_name: "Computer Science",
-      school_major: "Computer Science",
-      school_department: "Programming",
-      school_location: "Example University",
-    },
-  ];
+export default function ApprovedCertificate({ userId }) {
+  const [certificates, setCertificates] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    api
+      .get(`/users/${userId}/certificate`)
+      .then((response) => {
+        setCertificates(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [userId]);
 
   return (
-    <div>
+    <div className="user-certificates">
       <h2>Certificates</h2>
-      <ul>
-        {certificates.map((certificate, index) => (
-          <li key={index}>
-            <Certificate {...certificate} />
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <LoadingAnimation />
+      ) : certificates.filter(
+          (certificate) => certificate.verified_certificate === true
+        ).length > 0 ? (
+        <ul>
+          {certificates
+            .filter((certificate) => certificate.verified_certificate === true)
+            .map((certificate, index) => (
+              <li key={index}>
+                <Certificate {...certificate} />
+              </li>
+            ))}
+        </ul>
+      ) : (
+        <p className="no-certificates">No certificates found</p>
+      )}
     </div>
   );
 }
