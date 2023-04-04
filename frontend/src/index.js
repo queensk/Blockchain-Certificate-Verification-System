28d604010593cba6a59f1b14b2e82eace22bd9f2
queensk <queenskisivuli@gmail.com>
2023-04-04 10:45:13 +0300

    update data providers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,11 +8,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
 
