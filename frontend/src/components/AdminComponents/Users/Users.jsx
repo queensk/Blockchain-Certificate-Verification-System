7ad27eb0d9e0c5c41a742d3029173680e8aa8a5a
queensk <queenskisivuli@gmail.com>
2023-04-04 10:38:23 +0300

    user data

---
+++ frontend/src/components/AdminComponents/Users/Users.jsx
@@ -0,0 +1,160 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../../theme";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import PersonIcon from "@mui/icons-material/Person";
+import Header from "../Header/Header";
+import { useEffect, useState } from "react";
+import api from "../../../api/api";
+
+export default function Users() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/users")
+      .then((response) => {
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "first_name",
+      headerName: "First Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "last_name",
+      headerName: "Last Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "phone_number",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "role",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: ({ row: { role } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+              role === "admin"
+                ? colors.greenAccent[600]
+                : role === "manager"
+                ? colors.greenAccent[700]
+                : colors.greenAccent[700]
+            }
+            borderRadius="4px"
+          >
+            {role === "admin" && <AdminPanelSettingsOutlinedIcon />}
+            {role === "manager" && <SecurityOutlinedIcon />}
+            {role === "user" && <PersonIcon />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {role}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "delete",
+      headerName: "Delete",
+      sortable: false,
+      width: 100,
+      disableClickEventBubbling: true,
+      renderCell: ({ row }) => {
+        const handleDelete = () => {
+          console.log(row.id)
+          api
+            .delete(`/users/${row.id}`)
+            .then(() => {
+              setUserData((prevData) =>
+                prevData.filter((user) => user.id !== row.id)
+              );
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        };
+
+        return (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+            onClick={handleDelete}
+          >
+            <DeleteIcon color="error" />
+          </Box>
+        );
+      },
+    },
+  ];
+  return (
+    <Box m="20px">
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          checkboxSelection
+          rows={userData}
+          columns={columns}
+          components={{ Toolbar: GridToolbar }}
+        />
+      </Box>
+    </Box>
+  );
+}
