44a93b9f6d433342cb882dda6b9cb6fb3061b117
queensk <queenskisivuli@gmail.com>
2023-04-04 10:48:49 +0300

    test certificate contaract

--- Backend/contracts/test/Certificate.test.js
+++ Backend/contracts/test/Certificate.test.js
@@ -1,47 +1,37 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+it("should add a certificate and emit an event", async function () {
+  const name = "John Doe";
+  const schoolName = "Hardhat University";
+  const schoolMajor = "Computer Science";
+  const schoolDepartment = "Engineering";
 
-describe("Certificate contract", function () {
-  let certificateContract;
+  const certificateHash = ethers.utils.formatBytes32String("Certificate Hash");
 
-  beforeEach(async function () {
-    const Certificate = await ethers.getContractFactory("Certificate");
-    certificateContract = await Certificate.deploy();
-    await certificateContract.deployed();
-  });
+  const tx = await certificateContract.addCertificate(
+    name,
+    schoolName,
+    schoolMajor,
+    schoolDepartment,
+    certificateHash
+  );
+  const receipt = await tx.wait();
+  const certificateData = receipt.events[0].args[0];
+  expect(certificateData.studentName).to.equal(name);
+  expect(certificateData.schoolName).to.equal(schoolName);
+  expect(certificateData.schoolMajor).to.equal(schoolMajor);
+  expect(certificateData.schoolDepartment).to.equal(schoolDepartment);
+  expect(certificateData.verified).to.equal(false);
+});
 
-  it("should add and verify certificate correctly", async function () {
-    // Add a new certificate
-    const certName = "John Doe";
-    const certDate = "2022-02-25";
-    const certIssuer = "Example Organization";
-    const certHash = ethers.utils.formatBytes32String("0x1234");
-    await certificateContract.addCertificate(
-      certName,
-      certDate,
-      certIssuer,
-      certHash
-    );
-
-    // Verify the certificate
-    const certificateData = await certificateContract.getCertificate(certHash);
-    expect(certificateData.name).to.equal(certName);
-    expect(certificateData.date).to.equal(certDate);
-    expect(certificateData.issuer).to.equal(certIssuer);
-    expect(certificateData.hash).to.equal(certHash);
-    console.log(certHash);
-    console.log(certificateData);
-
-    const verified = await certificateContract.verifyCertificate(certHash);
-    expect(verified).to.be.true;
-  });
-
-  it("should return empty certificate for non-existent hash", async function () {
-    const certHash = ethers.utils.formatBytes32String("");
-    const certificateData = await certificateContract.getCertificate(certHash);
-    expect(certificateData.name).to.be.empty;
-    expect(certificateData.date).to.be.empty;
-    expect(certificateData.issuer).to.be.empty;
-    expect(certificateData.hash).to.equal(certHash);
-  });
+it("should return empty certificate for non-existent hash", async function () {
+  const certificateHash = ethers.utils.formatBytes32String(
+    "Non-existent Certificate Hash"
+  );
+  const certificateData = await certificateContract.getCertificateInfo(
+    certificateHash
+  );
+  expect(certificateData.studentName).to.equal("");
+  expect(certificateData.schoolName).to.equal("");
+  expect(certificateData.schoolMajor).to.equal("");
+  expect(certificateData.schoolDepartment).to.equal("");
+  expect(certificateData.verified).to.equal(false);
 });
