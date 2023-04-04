dabd8719f965402e0f02c2ca817fe6e8dcbcb34f
queensk <queenskisivuli@gmail.com>
2023-04-04 10:04:04 +0300

    protected components

--- frontend/src/components/RequireAuth/RequireAuth.jsx
+++ frontend/src/components/RequireAuth/RequireAuth.jsx
@@ -6,13 +6,24 @@ const RequireAuth = ({ allowedRoles }) => {
   const { authenticated, userRole } = useContext(AuthContext);
   const location = useLocation();
 
-  return [...userRole].find((role) => allowedRoles?.includes(role)) ? (
+  return authenticated && [...userRole].find((role) => allowedRoles?.includes(role)) ? (
     <Outlet />
   ) : authenticated ? (
     <Navigate to="/unauthorized" state={{ from: location }} replace />
   ) : (
-    <Navigate to="/" state={{ from: location }} replace />
+    <Navigate to="/login" state={{ from: location }} replace />
   );
 };
 
 export default RequireAuth;
+// authenticated && userRole === "user" ? (
+//   <Navigate to="/user/dashboard" state={{ from: location }} replace />
+// ) : authenticated && userRole === "school" ? (
+//   <Navigate to="/school/dashboard" state={{ from: location }} replace />
+// ) : authenticated && userRole === "admin" ? (
+//   <Navigate to="/school/dashboard" state={{ from: location }} replace />
+// ) : authenticated ? (
+//   <Navigate to="/unauthorized" state={{ from: location }} replace />
+// ) : (
+//   <Navigate to="/" state={{ from: location }} replace />
+// );
