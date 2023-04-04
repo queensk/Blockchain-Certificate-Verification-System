0c24f0bbe1676b978e87ec04d5d891cf15c90153
queensk <queenskisivuli@gmail.com>
2023-04-04 10:23:15 +0300

    upload file update

--- frontend/src/components/UserCertificates/UserCertificates.jsx
+++ frontend/src/components/UserCertificates/UserCertificates.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import "./UserCertificates.css";
 import UploadFile from "../UploadFile/UploadFile";
 
-export default function UserCertificates({ userId, firstName, lastName }) {
+export default function UserCertificates({ userId, firstName, userRole, lastName }) {
   return (
     <div className="user-certificates">
       <h1>Upload PDF certificate for verifications</h1>
-      <UploadFile userId={userId} firstName={firstName} lastName={lastName} />
+      <UploadFile
+        userId={userId}
+        firstName={firstName}
+        lastName={lastName}
+        userRole={userRole}
+      />
     </div>
   );
 }
