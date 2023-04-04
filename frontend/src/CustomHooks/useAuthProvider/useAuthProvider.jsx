f578f320675fe0a9759a30c461c737e47fcc3a50
queensk <queenskisivuli@gmail.com>
2023-04-04 10:40:25 +0300

    user AuthProvider

--- frontend/src/CustomHooks/useAuthProvider/useAuthProvider.jsx
+++ frontend/src/CustomHooks/useAuthProvider/useAuthProvider.jsx
@@ -2,45 +2,89 @@ import { useState, useEffect } from "react";
 import jwtDecode from "jwt-decode";
 
 function useAuthenticator() {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [userId, setUserId] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-  const [token, setToken] = useState(localStorage.getItem("appCertificate"));
-  const [userRole, setUserRole] = useState("");
+  const getToken = () => {
+    const tokenString = localStorage.getItem("appCertificate");
+    // const userToken = JSON.parse(tokenString);
+    return tokenString;
+  };
+
+  const decodeToken = (token) => {
+    try {
+      const decode = jwtDecode(token);
+      return decode;
+    } catch (error) {
+      console.error("Error decoding token:", error);
+      return;
+    }
+  };
+
+  const checkExp = (exp) => {
+    const currentTime = new Date().getTime() / 1000;
+    return exp > currentTime ? true : false;
+  };
+
+  const [token, setToken] = useState(getToken());
+  const [decode, setDecode] = useState(decodeToken(token));
+  const [authenticated, setAuthenticated] = useState(checkExp(decode?.exp));
+  const [firstName, setFirstName] = useState(decode?.first_name);
+  const [lastName, setLastName] = useState(decode?.last_name);
+  const [userId, setUserId] = useState(decode?.user_id);
+  const [userEmail, setUserEmail] = useState(decode?.user_email);
+  const [userRole, setUserRole] = useState(decode?.role);
 
   useEffect(() => {
+    setToken(getToken());
     if (token) {
-      const decode = jwtDecode(token);
-      const currentTime = new Date().getTime() / 1000;
-      if (decode.exp > currentTime) {
-        setAuthenticated(true);
-        setFirstName(decode.first_name);
-        setLastName(decode.last_name);
-        setUserId(decode.user_id);
-        setUserEmail(decode.user_email);
-        setUserRole(decode.role);
+      console.log(token);
+      console.log(userRole);
+      console.log(authenticated);
+      console.log(firstName);
+      // const decode = decodeToken(token);
+      // const currentTime = new Date().getTime() / 1000;
+      if (checkExp(decode?.exp)) {
+        if (decode?.role !== "school") {
+          setAuthenticated(true);
+          setFirstName(decode?.first_name);
+          setLastName(decode?.last_name);
+          setUserId(decode?.user_id);
+          setUserEmail(decode?.user_email);
+          setUserRole(decode?.role);
+        } else {
+          setAuthenticated(true);
+          setFirstName(decode?.first_name);
+          setUserEmail(decode?.user_email);
+          setUserRole(decode?.role);
+          setUserId(decode?.user_id);
+        }
       } else {
         setAuthenticated(false);
         setFirstName(null);
         setLastName(null);
-        localStorage.clear();
+        // localStorage.clear();
       }
     } else {
-      localStorage.clear();
+      // localStorage.clear();
       setAuthenticated(false);
       setFirstName("");
       setLastName("");
       setUserId("");
       setUserEmail("");
     }
-    if (!token) {
-      setToken(localStorage.getItem("appCertificate"));
-    }
-  }, [token]);
-
+    // if (!token) {
+    //   setToken(localStorage.getItem("appCertificate"));
+    // }
+  }, [
+    token,
+    authenticated,
+    firstName,
+    lastName,
+    userId,
+    userRole,
+    setAuthenticated,
+    setToken,
+  ]);
   return {
+    token,
     authenticated,
     firstName,
     lastName,
