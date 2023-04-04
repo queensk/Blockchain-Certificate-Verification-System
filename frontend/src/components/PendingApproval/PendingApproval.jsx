bfdee0011cdb0b3e29f2d06082a83e9d0a872588
queensk <queenskisivuli@gmail.com>
2023-04-04 10:03:32 +0300

    peing approvals update

--- frontend/src/components/PendingApproval/PendingApproval.jsx
+++ frontend/src/components/PendingApproval/PendingApproval.jsx
@@ -1,39 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Certificate from "../Certificate/Certificate";
+import api from "../../api/api";
+import LoadingAnimation from "../Loading/Loading";
 
-export default function PendingApproval() {
-  const certificates = [
-    {
-      student_name: "John Smith",
-      student_email: "john.smith@example.com",
-      school_name: "Computer Science",
-      school_major: "Computer Science",
-      school_department: "Programming",
-      school_location: "Example University",
-    },
-  ];
+export default function PendingApproval({ userId, userRole }) {
+  const [certificates, setCertificates] = useState([]);
+  const [certificateUpdate, setCertificateUpdate] = useState();
+  const [loading, setLoading] = useState(true);
 
-  const [certificateList, setCertificateList] = useState(certificates);
+  useEffect(() => {
+    api
+      .get(`/users/${userId}/certificate`)
+      .then((response) => {
+        setCertificates(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [userId, certificateUpdate]);
 
-  const handleVerify = (index) => {
-    const newCertificateList = [...certificateList];
-    newCertificateList[index].verified = true;
-    setCertificateList(newCertificateList);
+  const handleRequestVerification = (certificateId) => {
+    const verify = {
+      certificate_status: "pending",
+    };
+
+    api
+      .post(`/certificate/${certificateId}/request/verify`, verify)
+      .then((response) => {
+        setCertificateUpdate(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
-    <div>
+    <div className="user-certificates">
       <h2>Certificates</h2>
-      <ul>
-        {certificateList.map((certificate, index) => (
-          <li key={index}>
-            <Certificate {...certificate} />
-            <button onClick={() => handleVerify(index)}>
-              Request Verification
-            </button>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <LoadingAnimation />
+      ) : certificates.filter(
+          (certificate) => certificate.verified_certificate === false
+        ).length === 0 ? (
+        <p className="no-certificates">No certificates found</p>
+      ) : (
+        <ul>
+          {certificates
+            .filter((certificate) => certificate.verified_certificate === false)
+            .map((certificate, index) => (
+              <li key={index}>
+                <Certificate
+                  {...certificate}
+                  handleRequestVerification={handleRequestVerification}
+                  userRole={userRole}
+                  setCertificateUpdate={setCertificateUpdate}
+                />
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   );
 }
