2863ca1661208ccacd28d34c91021036703b2052
queens kisivuli <queenskisivuli@gmail.com>
2023-04-07 17:36:06 +0300

    Admin user create

--- frontend/src/components/AdminComponents/UserForm/UserForm.jsx
+++ frontend/src/components/AdminComponents/UserForm/UserForm.jsx
@@ -1,115 +1,129 @@
+import React, { useState } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../../components/AdminComponents/Header/Header";
 import api from "../../../api/api";
+import MessagePopUp from "../../MessagePopUp/MessagePopUp";
 
 const UserForm = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
+  const [error, setError] = useState("");
+
   const handleFormSubmit = (values) => {
-    const data = JSON.stringify(values); // convert values to JSON format
+    const { first_name, last_name, ...rest } = values;
+    const username = `${first_name.toLowerCase()}${last_name.toLowerCase()}`;
+
+    const data = JSON.stringify({ username, ...rest });
+    console.log(data);
     api
-      .post("/users", data, {
+      .post("/admins", data, {
         headers: {
-          "Content-Type": "application/json", // set the content type to JSON
+          "Content-Type": "application/json",
         },
       })
       .then((response) => {
-        console.log(response.data); // handle the response from the server
+        setError(`success creating ${response.data.email}`);
       })
       .catch((error) => {
-        console.error(error); // handle any errors that occur during the request
+        if (error) {
+          setError("An error occurred try again !!");
+        }
+        console.error(error);
       });
   };
 
   return (
-    <Box m="20px">
-      <Header title="CREATE USER" subtitle="Create a New User Profile" />
-      <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={checkoutSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <Box
-              display="grid"
-              gap="30px"
-              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-              sx={{
-                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-              }}
-            >
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="First Name"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.first_name}
-                name="first_name"
-                error={!!touched.first_name && !!errors.first_name}
-                helperText={touched.first_name && errors.first_name}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Last Name"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.last_name}
-                name="last_name"
-                error={!!touched.last_name && !!errors.last_name}
-                helperText={touched.last_name && errors.last_name}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Email"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.email}
-                name="email"
-                error={!!touched.email && !!errors.email}
-                helperText={touched.email && errors.email}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="password"
-                label="Password"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.password}
-                name="password"
-                error={!!touched.password && !!errors.password}
-                helperText={touched.password && errors.password}
-                sx={{ gridColumn: "span 4" }}
-              />
-            </Box>
-            <Box display="flex" justifyContent="end" mt="20px">
-              <Button type="submit" color="secondary" variant="contained">
-                Create New User
-              </Button>
-            </Box>
-          </form>
-        )}
-      </Formik>
-    </Box>
+    <>
+      {error && <MessagePopUp message={error} setMessage={setError} />}
+      <Box m="20px">
+        <Header title="CREATE USER" subtitle="Create a New User Profile" />
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValues}
+          validationSchema={checkoutSchema}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Box
+                display="grid"
+                gap="30px"
+                gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                sx={{
+                  "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                }}
+              >
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.first_name}
+                  name="first_name"
+                  error={!!touched.first_name && !!errors.first_name}
+                  helperText={touched.first_name && errors.first_name}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.last_name}
+                  name="last_name"
+                  error={!!touched.last_name && !!errors.last_name}
+                  helperText={touched.last_name && errors.last_name}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email}
+                  name="email"
+                  error={!!touched.email && !!errors.email}
+                  helperText={touched.email && errors.email}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="password"
+                  label="Password"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.password}
+                  name="password"
+                  error={!!touched.password && !!errors.password}
+                  helperText={touched.password && errors.password}
+                  sx={{ gridColumn: "span 4" }}
+                />
+              </Box>
+              <Box display="flex" justifyContent="end" mt="20px">
+                <Button type="submit" color="secondary" variant="contained">
+                  Create New User
+                </Button>
+              </Box>
+            </form>
+          )}
+        </Formik>
+      </Box>
+    </>
   );
 };
 // const phoneRegExp =
